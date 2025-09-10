08a4779fde52e176d24dfed51df89aee89ab56bc
Francis Anwuzia <francisanwuzia3@gmail.com>
2025-09-10 15:39:02 +0200

    Fix Browser loading issue

--- app.js
+++ app.js
@@ -139,7 +139,7 @@ app.use((req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
 
 module.exports = app;
